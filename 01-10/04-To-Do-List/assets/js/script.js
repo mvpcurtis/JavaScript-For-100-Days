ac9ef802a3255049ba2e19d482bb08072ef0383c
mvpcurtis <curt.is@tuta.io>
2025-04-17 22:14:20 +0100

    4th project started. A to-do list

---
+++ 01-10/04-To-Do-List/assets/js/script.js
@@ -0,0 +1,10 @@
+console.log('Connected.');
+
+const addBtn = document.querySelector('button');
+const inputFld = document.querySelector('input');
+
+const handler = () => {};
+
+addBtn.addEventListener('click', function () {
+  handler();
+});
