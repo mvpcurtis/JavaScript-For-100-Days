67e6a7f3bd602b0b2a31dbb595459868821b6498
mvpcurtis <curt.is@tuta.io>
2025-04-18 22:10:41 +0100

    5th is complete. A counter which stores the value in local storage.

--- 01-10/04-To-Do-List/assets/js/script.js
+++ 01-10/04-To-Do-List/assets/js/script.js
@@ -3,7 +3,19 @@ console.log('Connected.');
 const addBtn = document.querySelector('button');
 const inputFld = document.querySelector('input');
 
-const handler = () => {};
+const addTodo = () => {
+  let el = document.createElement('li');
+  el.textContent = inputFld.value;
+  document.querySelector('ul').appendChild(el);
+};
+
+const handler = () => {
+  addTodo();
+};
+
+document.querySelector('li').addEventListener('dblclick', function () {
+  document.querySelector('li').remove();
+});
 
 addBtn.addEventListener('click', function () {
   handler();
