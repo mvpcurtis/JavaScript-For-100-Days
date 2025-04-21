1bf227da1dd38a9a1aa54e4191a829b28cd9dde1
mvpcurtis <curt.is@tuta.io>
2025-04-21 19:41:23 +0100

    Functionality is working. DOM toggling.

--- 01-10/08-Modal-Window/assets/js/script.js
+++ 01-10/08-Modal-Window/assets/js/script.js
@@ -1,1 +1,19 @@
 console.log('Connected.');
+
+const initModalBtn = document.querySelector('#init-modal');
+const closeModalBtn = document.querySelector('#close-modal');
+const overlay = document.querySelector('#overlay');
+const modalWindow = document.querySelector('#modal-window');
+
+const closeModal = () => {
+  overlay.style.display = 'none';
+  modalWindow.style.display = 'none';
+};
+
+const initModal = () => {
+  overlay.style.display = 'block';
+  modalWindow.style.display = 'block';
+};
+
+initModalBtn.addEventListener('click', initModal);
+closeModalBtn.addEventListener('click', closeModal);
