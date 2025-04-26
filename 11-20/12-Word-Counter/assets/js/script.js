e02a12c576c1c98a48154a7c05b2c816b99991aa
mvpcurtis <curt.is@tuta.io>
2025-04-26 16:34:05 +0100

    Created the word count function which returns the word length.

--- 11-20/12-Word-Counter/assets/js/script.js
+++ 11-20/12-Word-Counter/assets/js/script.js
@@ -11,7 +11,12 @@ const charCounter = (v) => {
   return v.length;
 };
 
+const wordCounter = (v) => {
+  return v.trim().split(/\s+/).length;
+};
+
 textarea.addEventListener('keyup', function () {
   value = textarea.value;
   charCountEl.textContent = `Chracter Counter: ${charCounter(value)}`;
+  wordCountEl.textContent = `Word Counter: ${wordCounter(value)}`;
 });
