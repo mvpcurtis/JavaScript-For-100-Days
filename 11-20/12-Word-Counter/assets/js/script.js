61169b77847737ee265fb67228f1b1227004e474
mvpcurtis <curt.is@tuta.io>
2025-04-26 16:24:16 +0100

    Created a character count function and refactored some styling.

--- 11-20/12-Word-Counter/assets/js/script.js
+++ 11-20/12-Word-Counter/assets/js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 console.log('Connected.');
+// word counter
+
+const textarea = document.querySelector('textarea');
+const charCountEl = document.querySelector('#character-counter');
+const wordCountEl = document.querySelector('#word-counter');
+let value;
+
+const charCounter = (v) => {
+  return v.length;
+};
+
+textarea.addEventListener('keyup', function () {
+  value = textarea.value;
+  charCountEl.textContent = `Chracter Counter: ${charCounter(value)}`;
+});
