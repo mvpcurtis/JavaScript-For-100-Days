8d2f231bc1240d66c731dc8b301ce1f0616fc883
mvpcurtis <curt.is@tuta.io>
2025-04-26 21:48:27 +0100

    12 project is functioning. To come back and possibly restyle.

--- 11-20/12-Word-Counter/assets/js/script.js
+++ 11-20/12-Word-Counter/assets/js/script.js
@@ -12,11 +12,14 @@ const charCounter = (v) => {
 };
 
 const wordCounter = (v) => {
+  if (v.trim() === '') {
+    return 0;
+  }
   return v.trim().split(/\s+/).length;
 };
 
 textarea.addEventListener('keyup', function () {
   value = textarea.value;
-  charCountEl.textContent = `Chracter Counter: ${charCounter(value)}`;
-  wordCountEl.textContent = `Word Counter: ${wordCounter(value)}`;
+  charCountEl.textContent = `Chracter Counter: ${charCounter(value) === 0 ? 0 : charCounter(value)}`;
+  wordCountEl.textContent = `Word Counter: ${wordCounter(value) === 0 ? 0 : wordCounter(value)}`;
 });
